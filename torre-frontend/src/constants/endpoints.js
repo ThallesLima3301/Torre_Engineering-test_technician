72e36bafb8b93170cc3ecda5a003d64dbb6d8df1
Thalles Lima <thalles.lima3301@gmail.com>
2025-06-13 12:55:58 -0300

    front-end

--- torre-frontend/src/constants/endpoints.js
+++ torre-frontend/src/constants/endpoints.js
@@ -1,6 +1,6 @@
 // src/constants/endpoints.js
 export const TORRE_ENDPOINTS = {
-  SEARCH_JOBS: '/api/torre/search',
+  SEARCH_JOBS: '/api/torre/jobs',
   GENOME: '/api/torre/genome',
   FAVORITES: '/api/torre/favorites',
   ANALYTICS: '/api/torre/analytics',
